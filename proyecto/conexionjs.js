7b8f6ac90ad091c53c3f0f727d4d7fe8dd377b98
ivan <Ivhanfch1@gmail.com>
2025-03-29 22:32:13 -0700

    c

---
+++ proyecto/conexionjs.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBVYMeMZsn_hxSyoYVopGcgx9ydr0BhxDY",
+    authDomain: "proyectocecyte-9f75e.firebaseapp.com",
+    databaseURL: "https://proyectocecyte-9f75e-default-rtdb.firebaseio.com",
+    projectId: "proyectocecyte-9f75e",
+    storageBucket: "proyectocecyte-9f75e.firebasestorage.app",
+    messagingSenderId: "478449984037",
+    appId: "1:478449984037:web:34110861ef0289292cd5cc",
+    measurementId: "G-DV46P0XEV0"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  const database = firebase.database();
